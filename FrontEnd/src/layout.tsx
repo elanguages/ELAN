340323fc2126a1285cea7c1e14d56772fc19b7b5
MarcuEduardA3 <75856169+MEinfo724@users.noreply.github.com>
2025-01-26 01:14:45 +0200

    Small animation added on change of page

--- FrontEnd/src/layout.tsx
+++ FrontEnd/src/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { VStack, Box } from "@chakra-ui/react";
 
 import { Footer, Navbar } from "./shared";
+import { motion } from "framer-motion";
 
 export const RootLayout: React.FC<React.PropsWithChildren<{}>> = ({
   children,
@@ -9,7 +10,19 @@ export const RootLayout: React.FC<React.PropsWithChildren<{}>> = ({
   return (
     <VStack minHeight="100vh" align="stretch">
       <Navbar />
-      <Box flex="1">{children}</Box>
+
+      <Box flex="1">
+        {/* If there is an issue just delete this */}
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.7 }}
+        >
+          {children}
+        </motion.div>
+      </Box>
+
       <Footer />
     </VStack>
   );
