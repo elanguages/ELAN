0e500dac74cdb140b8b123e590a716e0af4042f3
MarcuEduardA3 <75856169+MEinfo724@users.noreply.github.com>
2025-01-28 16:49:33 +0200

    Error handling on request + special error handling for SPARQL console endpoint

--- FrontEnd/src/shared/utils/handleRequestError.ts
+++ FrontEnd/src/shared/utils/handleRequestError.ts
@@ -1,5 +1,42 @@
 import { AxiosError } from "axios";
+import Swal from "sweetalert2";
+
+interface SPARQLQuerryError {
+  error: string;
+  validationError?: string;
+  details?: string;
+}
+
+const isSPARQLQuerryError = (data: any): data is SPARQLQuerryError => {
+  return (
+    typeof data === "object" &&
+    data !== null &&
+    typeof data.error === "string" &&
+    (typeof data.validationError === "string" ||
+      data.validationError === undefined)
+  );
+};
 
 export const handleRequestError = (error: AxiosError) => {
-  alert(error);
+  const responseData = error?.response?.data;
+
+  if (isSPARQLQuerryError(responseData)) {
+    Swal.fire({
+      title: `${responseData.error}`,
+      text: `${responseData.validationError}`,
+      icon: "error",
+      confirmButtonText: "Ok",
+      confirmButtonColor: "black",
+    });
+  } else {
+    Swal.fire({
+      title: `Error`,
+      text: `${error}`,
+      icon: "error",
+      confirmButtonText: "Ok",
+      confirmButtonColor: "black",
+    });
+  }
+
+  console.error(error);
 };
