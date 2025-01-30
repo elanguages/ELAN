f7acefc37ea6da2574c39e0180f6b34df97732be
MarcuEduardA3 <75856169+MEinfo724@users.noreply.github.com>
2025-01-30 23:12:35 +0200

    Ugly presentation page

--- FrontEnd/src/elan-presentation/constants.ts
+++ FrontEnd/src/elan-presentation/constants.ts
@@ -1,9 +1,8 @@
-export const PRESENTATION_TEXT = {
-  firstText: `This is the presentation page of ELAN project.`,
-  secondText: `MIT License
+export const LICENSE = {
+  title: `MIT License
 
-Copyright (c) 2025 Faculty of Computer Science, Alexandru Ioan Cuza University
-
+Copyright (c) 2025 Faculty of Computer Science, UAIC`,
+  body: `
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -21,4 +20,23 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.`,
+};
+export const ELAN = {
+  title: `ELAN`,
+  body: `ELAN (Esoteric Language Analysis Network) is a web platform for exploring and analyzing esoteric programming languages (esolangs). It provides a centralized hub for information, resources, and tools, leveraging a conceptual model (ontology/thesaurus) built from existing esolang lists like Wikidata.
+  ELAN offers a SPARQL endpoint for querying the knowledge base, enabling detailed information retrieval about esolangs, their features, tools, and applications.  The platform also recommends esolangs based on user preferences, language categories, and pragmatic use cases.  ELAN aims to make esolangs more accessible for learning, programming, research, and general discovery.`,
+};
+
+export const SWAGGER = {
+  title: `Swagger`,
+  body: `
+Our project features a comprehensive API, designed for seamless integration and interaction.  We've leveraged the OpenAPI Specification (Swagger) to provide clear and interactive documentation.  This allows developers to easily understand and utilize our API endpoints, request parameters, and response formats.  For detailed information about our API, including interactive testing tools, please explore our Swagger documentation:`,
+  link: `https://en.wikipedia.org/wiki/Romania`,
+};
+
+export const SCHOLARLY = {
+  title: `Scholarly`,
+  body: `
+This project is informed by common practices and knowledge within the field of web development.  We've considered various approaches and techniques in our design and implementation.  For a general overview of related concepts, please see our Background Information page:`,
+  link: `https://en.wikipedia.org/wiki/Romania`,
 };
--- FrontEnd/src/elan-presentation/page.tsx
+++ FrontEnd/src/elan-presentation/page.tsx
@@ -1,6 +1,90 @@
-import { PRESENTATION_TEXT } from "./constants";
-import { PresentationCard } from "./components";
+import { Grid, GridItem, Text } from "@chakra-ui/react";
+import { LICENSE, SCHOLARLY, SWAGGER, ELAN } from "./constants";
+
 export const PresentationView = () => {
-  return <PresentationCard {...PRESENTATION_TEXT} />;
+  return (
+    <Grid
+      templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
+      padding={{ base: "5px 20px 0px 20px", "2xl": "40px 160px 0px 160px" }}
+    >
+      <GridItem
+        // bg="green.100"
+        display="flex-column"
+        alignItems="center"
+        justifyContent="center"
+        borderRight={{ base: "0px", md: "1px solid black" }}
+        borderBottom="1px solid black"
+        padding="10px"
+      >
+        <Text fontSize="xl" fontWeight="bold">
+          {SCHOLARLY.title}
+        </Text>
+        <Text>
+          <Text as="span">{SCHOLARLY.body}</Text>{" "}
+          <a
+            href={SCHOLARLY.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "underline" }}
+          >
+            here
+          </a>
+          .
+        </Text>
+      </GridItem>
+      <GridItem
+        // bg="red.100"
+        display="flex-column"
+        alignItems="center"
+        justifyContent="center"
+        borderBottom="1px solid black"
+        padding="10px"
+      >
+        <Text fontSize="xl" fontWeight="bold">
+          {SWAGGER.title}
+        </Text>
+
+        <Text>
+          <Text as="span">{SWAGGER.body}</Text>{" "}
+          <a
+            href={SWAGGER.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "underline" }}
+          >
+            here
+          </a>
+          .
+        </Text>
+      </GridItem>
+      <GridItem
+        // bg="blue.100"
+        display="flex-column"
+        alignItems="center"
+        justifyContent="center"
+        borderRight={{ base: "", md: "1px solid black" }}
+        borderBottom={{ base: "1px solid black", md: "0px" }}
+        padding="10px"
+      >
+        <Text fontSize="xl" fontWeight="bold">
+          {ELAN.title}
+        </Text>
+        <Text>{ELAN.body}</Text>
+      </GridItem>
+      <GridItem
+        // bg="yellow.100"
+        display="flex-column"
+        alignItems="center"
+        justifyContent="center"
+        padding="10px"
+      >
+        <Text fontSize="xl" fontWeight="bold">
+          {LICENSE.title}
+        </Text>
+        <Text>{LICENSE.body}</Text>
+      </GridItem>
+    </Grid>
+  );
 };
+
 export default PresentationView;
